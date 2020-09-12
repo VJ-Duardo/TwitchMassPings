73e5dc33c74515601c5706a76ce5339331a52fee
VJ-Duardo <olafstein501@gmail.com>
2020-09-12 19:31:54 +0200

    added more chatter type options

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var channelInput = document.getElementById('channel');
 var nameNumInput = document.getElementById('nameNum');
-var chatterTypeSelect = document.getElementById('chatterType');
+var chatterTypes = document.getElementsByClassName('chatterType');
 var outputArea = document.getElementById('output');
 
 var startButton = document.getElementById('startButton');
@@ -13,12 +13,17 @@ async function start(){
 	let chatterObj;
 
 	try{
-		let response = await fetch('https://cors-anywhere.herokuapp.com/https://tmi.twitch.tv/group/user/'+channelInput.value+'/chatters');
+		let response = await fetch('https://cors-anywhere.herokuapp.com/https://tmi.twitch.tv/group/user/'+channelInput.value.toLowerCase()+'/chatters');
 		chatterObj = await response.json();
 	} catch(e) {
 		outputArea.value = "Something went wrong";
 		return;
 	}
 	
-	outputArea.value = chatterObj.chatters[chatterTypeSelect.value].sort(() => Math.random() - 0.5).slice(0, parseInt(nameNumInput.value)).join(' ');
+	let nameList = [];
+	for (let i=0; i<chatterTypes.length; i++){
+		if (chatterTypes[i].checked)
+			nameList = nameList.concat(chatterObj.chatters[chatterTypes[i].value]);
+	}
+	outputArea.value = nameList.sort(() => Math.random() - 0.5).slice(0, parseInt(nameNumInput.value)).join(' ');
 }
