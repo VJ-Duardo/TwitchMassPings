9fc6642d6a5ba71cde010c4e0d46506a4eb12ca1
VJ-Duardo <olafstein501@gmail.com>
2020-09-12 21:48:45 +0200

    formatting

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ var startButton = document.getElementById('startButton');
 startButton.onclick = start;
 
 async function start(){
-	if (typeof channelInput.value === 'undefined' || channelInput.value === "")
-		return;
+    if (typeof channelInput.value === 'undefined' || channelInput.value === "")
+        return;
 
-	let chatterObj;
+    let chatterObj;
 
-	try{
-		let response = await fetch('https://cors-anywhere.herokuapp.com/https://tmi.twitch.tv/group/user/'+channelInput.value.toLowerCase()+'/chatters');
-		chatterObj = await response.json();
-	} catch(e) {
-		outputArea.value = "Something went wrong";
-		return;
-	}
-	
-	let nameList = [];
-	for (let i=0; i<chatterTypes.length; i++){
-		if (chatterTypes[i].checked)
-			nameList = nameList.concat(chatterObj.chatters[chatterTypes[i].value]);
-	}
-	outputArea.value = nameList.sort(() => Math.random() - 0.5).slice(0, parseInt(nameNumInput.value)).join(' ');
+    try{
+        let response = await fetch('https://cors-anywhere.herokuapp.com/https://tmi.twitch.tv/group/user/'+channelInput.value.toLowerCase()+'/chatters');
+        chatterObj = await response.json();
+    } catch(e) {
+        outputArea.value = "Something went wrong";
+        return;
+    }
+    
+    let nameList = [];
+    for (let i=0; i<chatterTypes.length; i++){
+        if (chatterTypes[i].checked)
+            nameList = nameList.concat(chatterObj.chatters[chatterTypes[i].value]);
+    }
+    outputArea.value = nameList.sort(() => Math.random() - 0.5).slice(0, parseInt(nameNumInput.value)).join(' ');
 }
